b4312fc0d7e5773925015f118d1cddc80f81f303
satria-ricky <rickygodhand@gmail.com>
2022-02-02 01:55:27 +1300

    a

--- application/views/v_dashboard/login.php
+++ application/views/v_dashboard/login.php
@@ -2,8 +2,30 @@
       <!-- Jumbotron -->
       <div class="jumbotron">
         <h1>Silahkan Login!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+
+        <div class="card" style="display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;">
+            <div class="card-body">
+              <div class="table-responsive">
+	              	<form class="mh-100" style="width: 300px;">
+					    <div class="form-floating">
+					    <label for="floatingInput">Username</label>
+					      <input type="email" class="form-control" placeholder="Username">
+					    </div>
+					    <div class="form-floating">
+					    <label for="floatingPassword">Password</label>
+					      <input type="password" class="form-control" placeholder="Password">
+					    </div>
+					    
+					    	<button style="margin-top: 25px;" class="btn btn-primary btn-sm" type="submit">Masuk</button>
+					  	
+					  </form>
+              </div>
+            </div>
+        </div>
       </div>
 
      
